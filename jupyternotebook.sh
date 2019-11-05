eb131f811e7567d96978b904494a59c3abf88026
chan@alumni.ncu.edu.tw <chan@alumni.ncu.edu.tw>
2019-11-05 19:32:57 +0800

    day9

--- jupyternotebook.sh
+++ jupyternotebook.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker run -it --rm -p 8888:8888 -e JUPYTER_ENABLE_LAB=yes -v "$PWD":/home/jovyan/work jupyter/datascience-notebook:9b06df75e445
+docker run -it --rm -p 8888:8888 -e JUPYTER_ENABLE_LAB=yes -v "$PWD":/home/jovyan/work jupyter/datascience-notebook
 
